976b1b44d4fa201221e78dacfd7917d87f5b2efc
isalevine <isa.loves.coding@gmail.com>
2019-11-10 15:22:11 -0800

    setting up sinatra server to send json repeatedly

--- sinatra-json-server/json_server.rb
+++ sinatra-json-server/json_server.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'sinatra/streaming'
 require 'json'
+require 'securerandom'  # use SecureRandom.uuid to generate new unique id
+
+name_array = ["Ryu", "Peco", "Rei", "Momo", "Garr", "Nina"]
+hp_array = [132, 71, 15, 1, 0, 325]
+magic_array = ["Frost", "Typhoon", "Magic Ball", "Ascension", "Rejuvinate", "Weretiger"]
 
 get '/' do
     i = 100
@@ -11,4 +17,22 @@ get '/' do
     
     content_type :json
     hash.to_json
+end
+
+
+get '/stream' do
+    content_type :json
+    
+    stream do |out|
+        # 60.times do
+        #     out.puts Time.now
+        #     out.puts "Hello World!", "How are you?"
+        #     out.write "Written #{out.pos} bytes so far!"
+        #     out.write "<br>"
+        #     out.flush
+        #     sleep 1
+        # end
+        hash = {"is_json?": true}
+        out << hash.to_json
+    end
 end
