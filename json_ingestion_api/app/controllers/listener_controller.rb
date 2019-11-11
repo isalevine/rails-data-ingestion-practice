73f9b0f8663d628aac2956b107dbe12950e1517c
isalevine <isa.loves.coding@gmail.com>
2019-11-10 21:24:59 -0800

    added readouts for dev.to help article

--- json_ingestion_api/app/controllers/listener_controller.rb
+++ json_ingestion_api/app/controllers/listener_controller.rb
@@ -2,18 +2,23 @@ require 'net/http'
 
 class ListenerController < ApplicationController
     def index
-        response.headers['Content-Type'] = "application/json"
-
         url = URI.parse('http://localhost:3000/broadcaster')
         req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) {|http|
-            http.request(req)
-        }
+        res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
 
-        char_array = res.body.split("\n")
-        char_array.each do |char_str|
-            json = JSON.parse(char_str)
-            Character.create("uuid": json["uuid"], "name": json["name"], "hp": json["hp"], "magic": json["magic"])
+        puts <<-READOUT
+
+    res.body:
+    ==============================
+    #{res.body}
+
+        READOUT
+
+        char_array = res.body.split("\n\n")
+        char_array.each do |data_str|
+            data_hash = eval(data_str.slice!(6..-1))    # slice to remove leading "data: " substring
+            char_hash = data_hash[:character]
+            Character.create("uuid": char_hash[:uuid], "name": char_hash[:name], "hp": char_hash[:hp], "magic": char_hash[:magic])
         end
     end
 end
--- rails_json_server/app/controllers/broadcaster_controller.rb
+++ rails_json_server/app/controllers/broadcaster_controller.rb
@@ -1,5 +1,3 @@
-require 'reloader/sse'
-
 class BroadcasterController < ApplicationController
     include ActionController::Live
 
@@ -9,17 +7,17 @@ class BroadcasterController < ApplicationController
         magic_array = ["Frost", "Typhoon", "Magic Ball", "Ascension", "Rejuvinate", "Weretiger"]
 
         response.headers['Content-Type'] = "text/event-stream"
-        sse = Reloader::SSE.new(response.stream)
+        sse = SSE.new(response.stream)
 
         begin
-            5.times do      # this will batch together 5 json objects as one string, which need to be parsed by reloader_controller
+            5.times do      
                 character_hash = {
                     "uuid": SecureRandom.uuid,
                     "name": name_array.sample,
                     "hp": hp_array.sample,
                     "magic": magic_array.sample
                 }
-                sse.write(character_hash)
+                sse.write({ character: character_hash })
                 sleep 1
             end
         rescue IOError
