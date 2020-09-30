40c0b2273e2e592c35c9dd185b6fac373e3a75a5
isalevine <isa.loves.coding@gmail.com>
2020-09-29 23:29:45 -0700

    refactored based on dev.to advice pointing at these docs: https://ruby-doc.org/stdlib-2.6.5/libdoc/net/http/rdoc/Net/HTTP.html#class-Net::HTTP-label-Streaming+Response+Bodies

--- json_ingestion_api/app/controllers/listener_controller.rb
+++ json_ingestion_api/app/controllers/listener_controller.rb
@@ -3,22 +3,26 @@ require 'net/http'
 class ListenerController < ApplicationController
     def index
         url = URI.parse('http://localhost:3000/broadcaster')
-        req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+        
+        Net::HTTP.start(url.host, url.port) do |http|
+            request = Net::HTTP::Get.new(url.to_s)
+            http.request(request) do |response|
 
-        puts <<-READOUT
+                puts <<-READOUT
 
-    res.body:
-    ==============================
-    #{res.body}
+                res.read_body:
+                ==============================
 
-        READOUT
+                READOUT
 
-        char_array = res.body.split("\n\n")
-        char_array.each do |data_str|
-            data_hash = eval(data_str.slice!(6..-1))    # slice to remove leading "data: " substring
-            char_hash = data_hash[:character]
-            Character.create("uuid": char_hash[:uuid], "name": char_hash[:name], "hp": char_hash[:hp], "magic": char_hash[:magic])
+                response.read_body do |data_str|
+                    if data_str != ""
+                        data_hash = eval(data_str.slice!(6..-1))    # slice to remove leading "data: " substring
+                        char_hash = data_hash[:character]
+                        Character.create("uuid": char_hash[:uuid], "name": char_hash[:name], "hp": char_hash[:hp], "magic": char_hash[:magic])
+                    end
+                end
+            end
         end
     end
 end
--- rails_json_server/app/controllers/broadcaster_controller.rb
+++ rails_json_server/app/controllers/broadcaster_controller.rb
@@ -7,14 +7,14 @@ class BroadcasterController < ApplicationController
         magic_array = ["Frost", "Typhoon", "Magic Ball", "Ascension", "Rejuvinate", "Weretiger"]
 
         response.headers['Content-Type'] = "text/event-stream"
-        # sse = SSE.new(response.stream)
+        sse = SSE.new(response.stream)
         
         # moving the .times do loop here does NOT result in 5 separate events...
         # ...in fact, only ONE character is generated and sent!
         5.times do  
             sse = SSE.new(response.stream)
             begin
-                # 5.times do      
+                5.times do      
                     character_hash = {
                         "uuid": SecureRandom.uuid,
                         "name": name_array.sample,
@@ -23,7 +23,7 @@ class BroadcasterController < ApplicationController
                     }
                     sse.write({ character: character_hash })
                     sleep 1
-                # end
+                end
             rescue IOError
                 # client disconnected
             ensure
